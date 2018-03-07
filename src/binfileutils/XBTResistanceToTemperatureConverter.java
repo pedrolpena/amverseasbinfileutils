80da64527f0dbb659b679fd56bd906c8d7f89e3c
pena <pena@pena.aoml.noaa.gov>
2018-03-07 17:46:17 -0500

    made the methods public

--- src/binfileutils/XBTResistanceToTemperatureConverter.java
+++ src/binfileutils/XBTResistanceToTemperatureConverter.java
@@ -17,7 +17,7 @@ public class XBTResistanceToTemperatureConverter {
      * @param resistance the resistance measured in ohms.
      * @return A single temperature point.
      */
-    double convertResistaceToTemperature(double resistance) {
+    public double convertResistaceToTemperature(double resistance) {
         double dnlr = Math.log(resistance);
         double temp = (1.0 / (0.001290123 + 0.00023322529 * dnlr
                 + 0.00000045791293 * dnlr * dnlr
@@ -27,13 +27,13 @@ public class XBTResistanceToTemperatureConverter {
     }
 
     /**
-     * Converts an array XBT thermistor resistance values read by the recording
-     * device to a temperature in degrees celcius.
+     * Converts an array of XBT thermistor resistance values read by the
+     * recording device to a temperature in degrees celcius.
      *
      * @param resistance an array of the resistances measured in ohms.
      * @return An array of temperatures.
      */
-    double[] convertResistaceToTemperature(double[] resistance) {
+    public double[] convertResistaceToTemperature(double[] resistance) {
         int len = resistance.length;
         double[] temps = new double[len];
 
